ea22a81ecdcfc796f84255d55758710536e2ec6b
cloud-bash <79466037+cloud-bash@users.noreply.github.com>
2022-05-17 12:26:23 -0400

    Update PigLatin.py

--- PigLatin.py
+++ PigLatin.py
@@ -8,6 +8,7 @@ def pig_latin(text):
     say += word + " "
     # Turn the list back into a phrase
   say = say.rstrip()
+    #rstrip removes the extra " " from the last iteration through the words List
   return say
 		
 print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
